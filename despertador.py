3e2e425c76e8b310891b68c01c727ddeb747ed0a
projeto.fgmp <projeto.fgmp@gmail.com>
2022-12-11 16:23:14 -0300

    second commit

--- despertador.py
+++ despertador.py
@@ -1,6 +1,15 @@
 import datetime
 import pygame
 
+# Pergunte ao usuário quais dias da semana o alarme deve tocar
+print("Em quais dias da semana o alarme deve tocar?")
+print("(Digite os números dos dias, separados por vírgula)")
+print("Seg - 0, Ter - 1, Qua - 2, Qui - 3, Sex - 4, Sab - 5, Dom - 6")
+dias_semana = input()
+
+# Converter a lista de dias da semana em números inteiros
+dias_semana = [int(dia) for dia in dias_semana.split(",")]
+
 # Pergunte ao usuário em que horário o alarme deve tocar
 print("Em que horário o alarme deve tocar?")
 print("(Digite o horário no formato HH:MM)")
@@ -22,7 +31,9 @@ while True:
     # Obtenha a data e hora atuais
     agora = datetime.datetime.now()
 
-    # Verifique se é hora de tocar o alarme
-    if agora.strftime("%H:%M") == horario:
-        # Tocar a música de alarme
-        pygame.mixer.music.play()
+    # Verifique se o dia da semana atual está na lista de dias em que o alarme deve tocar
+    if agora.weekday() in dias_semana:
+        # Verifique se é hora de tocar o alarme
+        if agora.strftime("%H:%M") == horario:
+            # Tocar a música de alarme
+            pygame.mixer.music.play()
